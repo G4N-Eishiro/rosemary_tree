82ca741bfb143806d1dd64c4723902540e0327dc
G4N Eishiro <53969490+G4N-Eishiro@users.noreply.github.com>
2026-09-20 23:29:55 +0800

    rosemary: add lineage_rosemary.mk
    Add makefile for LineageOS device 'rosemary' with product details.

---
+++ lineage_rosemary.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/rosemary/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_rosemary
+PRODUCT_DEVICE := rosemary
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2101K7BNY
+
+PRODUCT_SYSTEM_NAME := rosemary_global
+PRODUCT_SYSTEM_DEVICE := rosemary
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+WITH_GMS := true
+# Surface flinger boosting (Smoother scrolling, fewer frame drops but Keeps CPU slightly “awake”)
+SURFACE_FLINGER_BOOST := true
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="missi_phoneext4_global-user 13 TP1A.220624.014 V14.0.16.0.TKLMIXM release-keys" \
+    BuildFingerprint=Redmi/rosemary_global/rosemary:13/TP1A.220624.014/V14.0.16.0.TKLMIXM:user/release-keys \
+    SystemModel=$(PRODUCT_SYSTEM_DEVICE) \
+    SystemName=$(PRODUCT_SYSTEM_NAME) \
+    ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
